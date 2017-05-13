6c1ae7428b4bc99b8ad7e6fb744478ce618ee4c0
Patrick Chin <pchin@nvidia.com>
2017-05-13 14:50:55 +0800

    Refactored Search Bar to be a class instead of functional component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-    return <input />
+class SearchBar extends Component{
+    render() {
+        return <input />;
+    }
 }
 
 export default SearchBar;
+
+// NOTE
+//      import React, {Component} from 'react';
+// IS THE SAME AS
+//      import React from 'react';
+//      const Component = React.Component
+
+// SAME THING:
+/*
+import React from 'react';
+class SearchBar extends React.Component{
+    render() {
+        return <input />;
+    }
+}
+
+export default SearchBar;
+*/
