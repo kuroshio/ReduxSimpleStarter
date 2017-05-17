cc25f7c71288e4a10844d2abf3e1047d4708265b
Patrick Chin <pchin@nvidia.com>
2017-05-17 23:46:39 +0800

    Lecture 23 Add video list, intro to callbacks

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDljDeTQmJLm7c6CNDjFqWZqQuJ-1EJZY8';
-
-YTSearch({key:API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
 
 // Create a new component. This component should produce some HTML
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {videos:[]}
+
+        YTSearch({key:API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos})
+            //SAME: this.setState({videos: videos})  // key and property must be the same
+        });
+    }
     render() {
         return (
             <div>
                 <SearchBar/>
+                <VideoList videos={this.state.videos}/>
             </div>
         )
     }
