2937727a3b690c505c86eadfcc6d9c210813666d
Patrick Chin <pchin@nvidia.com>
2017-05-26 01:31:08 +0800

    Lecture 45 added an action creator and bound it to BookList Container

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';    // react-redux is the glue between react and redux, they're completely separate
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
 class BookList extends Component {
     renderList() {
@@ -20,7 +22,7 @@ class BookList extends Component {
     }
 }
 
-
+// take some state and map it to the props of our container
 function mapStateToProps(state) {
     // this is the glue between react and redux
     // state contains array of books and the active book
@@ -33,9 +35,24 @@ function mapStateToProps(state) {
     }
 }
 
+// take an action creator and make it available to be called inside the container as well
+
+// anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch) {
+    // whenever selectBook is called, the result should be passed to all of our reducers (using dispatch)
+    // when action creator (selectBook)_ is called, the result flows through dispatch function
+    // dispatch receives all these actions like a funnel and spits them back out to all the reducers in the application
+    // remember selelctbook just returns a plain JS object - container itself doesnt care about it, only reducers do
+    // so bindactioncreators/dispatch  makes sure it flows through all the reducers
+    return bindActionCreators({selectBook:selectBook}, dispatch)
+}
+
 // connect() takes a function (mapStateToProps) and component (BookList) and returns a container
 // a container is a component that is aware of the state that is contained by redux
 // we don't want to export the plain BookList container, we want to export the container
 // whenever application state changes the object in the state function (mapStateToProps) will be assigned as props to 
 // the component (BookList)
-export default connect(mapStateToProps)(BookList);
+
+// promote BookList from a component to a container - it needs to know about this new dispatch method, selectBook. Make
+// is available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
