7b6974d78d4f034b56bab4e1833e389b77bde9a3
Patrick Chin <pchin@nvidia.com>
2017-06-03 16:37:48 +0800

    Lecture 50 add review notes

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,22 @@ import React, {Component} from 'react';
 import BookList from '../containers/book-list';
 import BookDetail from '../containers/book-detail';
 
+
+// Review Notes:
+// Redux is in charge of managing application state, which is a single plain Javascript Object
+// Application state is completely different and separate from component state
+
+// Application state is formed by reducers
+// reducers all get tied together by combineReducers method
+// for each key in that object we assign one reducer which is responsible for creating that piece of state
+
+// reducers are responsible for changing application state over time, and do that through use of actions.
+// when action is dispatched it flows through all the diff reducers of application
+// each reducer has the option to return a diff piece of state than usual based on the type of action received
+
+// action creators are just simple functions that return an action
+// action is just a plain javascript object
+// actions must always have a type defined, and optionally have a payload or any number of properties
 export default class App extends Component {
     render() {
         return (
