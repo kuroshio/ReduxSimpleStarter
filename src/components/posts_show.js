fb6dc236b6cfeae807e7a05bcfb1899ed426709b
Patrick Chin <pchin@nvidia.com>
2017-06-28 00:24:24 +0800

    Lecture 144 create fetchpost action creator and wire it up to our reducer

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 
+// posts show needs to be responsible for fetching its own data
+// consider two scenarios - 1) user visits PostsIndex first - fetch list of all blog posts
+// 2) user visits PostsShow first with a particular ID - only want to fetch that post
+// must always assume user may visit any page in the app directly, not always through index
 class PostsShow extends Component {
     render() {
         return (
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,24 @@
 import _ from 'lodash';
-import {FETCH_POSTS} from '../actions'; // import from index.js so don't need to specify
+import {FETCH_POSTS, FETCH_POST} from '../actions'; // import from index.js so don't need to specify
 
 // receive the previous state and an action and do something with it
 // default the state to be an object
 export default function(state = {}, action) {
     switch (action.type) {
+        case FETCH_POST:
+            // we know that our  post piece of state is an object, so we're returning an object
+            // we want to ADD to our existing app level state instead of tossing away all posts we've fetched
+
+            // // ES5 code below
+            // const post = action.payload.data;
+            // const newState = {...state};
+            // newState[post.id] = post
+            // return newState
+
+            // ES 6 code below - identical to above
+            // key interpolation - we are not creating an array - whatever the variable action.payload.data.id is
+            // make a new key on this object, using this value, and set its value equal to action.payload.data
+            return { ...state, [action.payload.data.id]: action.payload.data }
         case FETCH_POSTS:
             console.log(action.payload.data); // [post1, post2]
 
